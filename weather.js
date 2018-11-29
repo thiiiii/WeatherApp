9c2325a4ae08a3871b048cd42843aa7bfef19e47
Thiii <fireter.trongthi@gmail.com>
2018-11-29 10:20:27 +0200

    Fix the JS

--- weather.js
+++ weather.js
@@ -200,10 +200,6 @@ function getDate(){
 //////////
 // Ask for permission of Notification
 
-Notification.requestPermission(function(status) {
-    console.log('Notification permission status:', status);
-});
-
 
 
 /* Project step 
